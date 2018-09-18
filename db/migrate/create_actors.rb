bf84bcfc18fbf4cfe35c08150ad4cf5aec0a4416
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-18 19:18:55 +0000

    automatically backed up by learn

--- db/migrate/create_actors.rb
+++ db/migrate/create_actors.rb
@@ -1,15 +1,8 @@
 class CreateActors < ActiveRecord::Migration
-  #define a change method in which to do the migration
   def change
-    create_table :actors do |t| #we get a block variable here for the table
-      #primary key of :id is created for us!
-      # defining columns is as simple as t.[datatype] :column
+    create_table :actors do |t|
       t.string :first_name
       t.string :last_name
-
-      # the above breaks down to
-      # "create a column called :call_letters on table t with type string
-      t.integer :channel
     end
   end
 end
